940fd3cf7195a84225ab0c274964ef9e89ff7fb2
mmdrezakarimi <mohammadrezakarimisavadjani@gmail.com>
2026-09-11 13:28:47 -0700

    RegisterPAge

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,33 @@
 //Pages
-const intro0 = document.getElementsByClassName("intro0")
-const intro1 = document.getElementsByClassName("intro1")
-const intro2 = document.getElementsByClassName("intro2")
-const intro3 = document.getElementsByClassName("intro3")
-const wlcPage = document.getElementsByClassName("wlcPage")
-const loginPage = document.getElementsByClassName("loginPage")
-//IntroButtons
-const start = document.getElementsByClassName("start")
-const back = document.getElementsByClassName("noneBut")
-const next = document.getElementsByClassName("mainBut")
-const arrowBut = document.getElementsByClassName("arrowBut")
-const loginBut = document.getElementsByClassName("loginBut")
+const intro0 = document.getElementsByClassName("intro0");
+const intro1 = document.getElementsByClassName("intro1");
+const intro2 = document.getElementsByClassName("intro2");
+const intro3 = document.getElementsByClassName("intro3");
+const wlcPage = document.getElementsByClassName("wlcPage");
+const loginPage = document.getElementsByClassName("loginPage");
+const registerPage = document.getElementsByClassName("registerPage")
+//Buttons
+const start = document.getElementsByClassName("start");
+const back = document.getElementsByClassName("noneBut");
+const next = document.getElementsByClassName("mainBut");
+const arrowBut = document.getElementsByClassName("arrowBut");
+const loginBut = document.getElementsByClassName("loginBut");
+const createBut = document.getElementsByClassName("createBut")
+const registerBut = document.getElementsByClassName("registerBut")
+const loginButRegisterPage = document.getElementsByClassName("loginButRegisterPage")
+//LoginInputs
+const userName = document.querySelector("#txt")
+const password = document.querySelector("#pwd")
+    // function checkInputs(){
+    //     if(userName.value !== "" && password.value !== "" && password.value.length >= 8 ){
+    //         loginBut[1].disabled = false;
+    //     } else{
+    //          loginBut[1].disabled = true;
+    //     }          
+    // } 
+
+//registerButs
+
 //skip
     back[0].addEventListener("click" , function(){
         intro1[0].classList.remove("active");
@@ -30,6 +47,8 @@ const loginBut = document.getElementsByClassName("loginBut")
         wlcPage[0].classList.remove("hidde");
         wlcPage[0].classList.add("active");
     })
+
+
 //Backs
     back[1].addEventListener("click" , function(){
         intro0[0].classList.add("active");
@@ -59,6 +78,12 @@ const loginBut = document.getElementsByClassName("loginBut")
     arrowBut[1].addEventListener("click" , function(){
     loginPage[0].classList.add("hidde");
     loginPage[0].classList.remove("active");
+    wlcPage[0].classList.add("active");  
+    wlcPage[0].classList.remove("hidde");  
+    })
+    arrowBut[2].addEventListener("click" , function(){
+    registerPage[0].classList.add("hidde");
+    registerPage[0].classList.remove("active");
     wlcPage[0].classList.add("active");  
     wlcPage[0].classList.remove("hidde");  
     })
@@ -95,4 +120,35 @@ const loginBut = document.getElementsByClassName("loginBut")
         loginPage[0].classList.add("active");
         loginPage[0].classList.remove("hidde");
     })
-console.log(arrowBut)
+    //loginButLoginPage
+        function checkInputs(){
+        if(userName.value == "Mmd" && password.value == "2719Mmd@" && password.value.length >= 8 ){
+            loginBut[1].disabled = false;
+        } else{
+             loginBut[1].disabled = true;
+        }          
+    }  
+    userName.addEventListener("input" , checkInputs);
+    password.addEventListener("input" , checkInputs);
+    //
+    loginButRegisterPage[0].addEventListener("click" , function(){
+        registerPage[0].classList.add("hidde")
+        registerPage[0].classList.remove("active")
+        loginPage[0].classList.add("active");
+        loginPage[0].classList.remove("hidde");
+    })
+//register&createBut
+    createBut[0].addEventListener("click" , function(){
+        wlcPage[0].classList.remove("active")
+        wlcPage[0].classList.add("hidde")
+        registerPage[0].classList.remove("hidde")
+        registerPage[0].classList.add("active")
+
+    })
+    registerBut[0].addEventListener("click" , function(){
+        loginPage[0].classList.remove("active");
+        loginPage[0].classList.add("hidde");
+        registerPage[0].classList.remove("hidde")
+        registerPage[0].classList.add("active")
+
+    })
