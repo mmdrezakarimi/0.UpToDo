2fdd3c902b84b13616550bcf9a440162de4ede8e
mmdrezakarimi <mohammadrezakarimisavadjani@gmail.com>
2026-09-10 23:24:42 -0700

    Intro 2

--- Js/script.js
+++ Js/script.js
@@ -5,6 +5,14 @@ const myTimeout = setTimeout(Loading1 , 3000);
 
 function Loading1(){
     intro1[0].classList.toggle("hidde");
-    intro2[0].classList.toggle("active")
+    intro2[0].classList.toggle("active");
 }
 console.time()
+//Intro 2 Button
+//Back
+const back = document.getElementsByClassName("noneBut")
+back[1].addEventListener("click" , function(){
+    intro1[0].classList.toggle("active");
+    intro2[0].classList.toggle("hidde");
+})
+console.log(back)
